12bb6d483e75984468d569c96066439c8a54d461
yangtaimeng <yangtaimeng@yidaoit.cn>
2021-02-25 22:50:33 +0800

    修改目录结构

---
+++ src/config/app.js
@@ -0,0 +1,18 @@
+
+let domain = 'https://api-dev.daodaobao.yidaoit.net'
+
+module.exports = {
+	HTTP_REQUEST_URL:domain,
+	HEADER:{
+		'content-type': 'application/json'
+	},
+	HEADERPARAMS:{
+		'content-type': 'application/x-www-form-urlencoded'
+	},
+	// 回话密钥名称 请勿修改此配置
+	TOKENNAME: 'Authori-zation',
+	// 缓存时间 0 永久
+	EXPIRE:0,
+	//分页最多显示条数
+	LIMIT: 10
+};
